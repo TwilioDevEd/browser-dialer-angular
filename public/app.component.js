ff9c2c4c247ea3fe6376b5e04b2c715e9e06c83f
mosampaio <mosampaio@gmail.com>
2016-10-13 10:54:34 -0300

    Fix buttons position

--- public/app.component.js
+++ public/app.component.js
@@ -24,46 +24,6 @@
         </ul>
       </div>
 
-      <!-- DTMF Tone interface -->
-      <div class="keys" *ngIf="onPhone">
-        <div class="key-row">
-          <button class="btn btn-circle btn-default" (click)="sendDigit('1')">1</button>
-          <button class="btn btn-circle btn-default" (click)="sendDigit('2')">2
-            <span>A B C</span>
-          </button>
-          <button class="btn btn-circle btn-default" (click)="sendDigit('3')">3
-            <span>D E F</span>
-          </button>
-        </div>
-        <div class="key-row">
-          <button class="btn btn-circle btn-default" (click)="sendDigit('4')">4
-            <span>G H I</span>
-          </button>
-          <button class="btn btn-circle btn-default" (click)="sendDigit('5')">5
-            <span>J K L</span>
-          </button>
-          <button class="btn btn-circle btn-default" (click)="sendDigit('6')">6
-            <span>M N O</span>
-          </button>
-        </div>
-        <div class="key-row">
-          <button class="btn btn-circle btn-default" (click)="sendDigit('7')">7
-            <span>P Q R S</span>
-          </button>
-          <button class="btn btn-circle btn-default" (click)="sendDigit('8')">8
-            <span>T U V</span>
-          </button>
-          <button class="btn btn-circle btn-default" (click)="sendDigit('9')">9
-            <span>W X Y Z</span>
-          </button>
-        </div>
-        <div class="key-row">
-          <button class="btn btn-circle btn-default" (click)="sendDigit('*')">*</button>
-          <button class="btn btn-circle btn-default" (click)="sendDigit('0')">0</button>
-          <button class="btn btn-circle btn-default" (click)="sendDigit('#')">#</button>
-        </div>
-      </div>
-
       <!-- Telephone input field -->
       <input type="tel" class="form-control" placeholder="555-666-7777"
           (keyup)="onNumberKeyUp($event)">
@@ -82,6 +42,46 @@
         <i class="fa fa-fw"
             [ngClass]="{'fa-microphone-slash': muted, 'fa-microphone': !muted}"></i>
       </button>
+    </div>
+
+    <!-- DTMF Tone interface -->
+    <div class="keys" *ngIf="onPhone">
+      <div class="key-row">
+        <button class="btn btn-circle btn-default" (click)="sendDigit('1')">1</button>
+        <button class="btn btn-circle btn-default" (click)="sendDigit('2')">2
+          <span>A B C</span>
+        </button>
+        <button class="btn btn-circle btn-default" (click)="sendDigit('3')">3
+          <span>D E F</span>
+        </button>
+      </div>
+      <div class="key-row">
+        <button class="btn btn-circle btn-default" (click)="sendDigit('4')">4
+          <span>G H I</span>
+        </button>
+        <button class="btn btn-circle btn-default" (click)="sendDigit('5')">5
+          <span>J K L</span>
+        </button>
+        <button class="btn btn-circle btn-default" (click)="sendDigit('6')">6
+          <span>M N O</span>
+        </button>
+      </div>
+      <div class="key-row">
+        <button class="btn btn-circle btn-default" (click)="sendDigit('7')">7
+          <span>P Q R S</span>
+        </button>
+        <button class="btn btn-circle btn-default" (click)="sendDigit('8')">8
+          <span>T U V</span>
+        </button>
+        <button class="btn btn-circle btn-default" (click)="sendDigit('9')">9
+          <span>W X Y Z</span>
+        </button>
+      </div>
+      <div class="key-row">
+        <button class="btn btn-circle btn-default" (click)="sendDigit('*')">*</button>
+        <button class="btn btn-circle btn-default" (click)="sendDigit('0')">0</button>
+        <button class="btn btn-circle btn-default" (click)="sendDigit('#')">#</button>
+      </div>
     </div>
 
     <!-- Status logging -->
